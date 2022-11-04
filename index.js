273a062aebfc6f009e59e1d907e28e86778ad181
aditya <adichoudhary2222@gmail.com>
2022-11-04 14:02:26 +0530

    AC - basic-setup

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const express = require("express");
 require("dotenv").config();
+const route = require("./app/route");
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(route);
+
 const PORT = process.env.SERVER_PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
