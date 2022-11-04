ebda81c6bc5d7247ebea007611a6df206721a680
aditya <adichoudhary2222@gmail.com>
2022-11-04 17:41:28 +0530

    AC - added fetch food collector

--- app/route.js
+++ app/route.js
@@ -6,15 +6,20 @@ app.use(router);
 //controllers
 const signup = require("./controller/signup");
 const login = require("./controller/login");
+const fetchFoodCollectors = require("./controller/fetchFoodCollectors");
 
 //routes
 
 router.post("/signup", signup, (req, res) => {
-    console.log("Signed Up");
-})
+  console.log("Signed Up");
+});
 
 router.post("/login", login, (req, res) => {
-    console.log("User Logged In!");
-})
+  console.log("User Logged In!");
+});
+
+router.get("/foodcollectors", fetchFoodCollectors, (req, res) => {
+  console.log("Fetched food collectors successfully!!");
+});
 
 module.exports = router;
