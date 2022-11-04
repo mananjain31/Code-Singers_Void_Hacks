8094575e2817273caf6a8e83dec61a169fe359c3
mananjain31 <mananjain31jan@gmail.com>
2022-11-04 14:56:20 +0530

    MJ - landing page structure and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RequireAuth from "components/wrappers/RequireAuth";
 import LandingPage from "pages/LandingPage";
 import LoginRegisterPage from "pages/LoginRegisterPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login-register" element={<LoginRegisterPage />} />
-      <Route element={<RequireAuth>Protected</RequireAuth>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login-register" element={<LoginRegisterPage />} />
+        <Route
+          path="/dashboard"
+          element={<RequireAuth>Protected</RequireAuth>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>CodeSingers</h1>
+          <button>Login</button>
+        </nav>
+        {/* Hero section */}
+        <section></section>
+      </header>
+      <main>
+        {/* About Indore Waste mangement */}
+        <section></section>
+        {/* Our Proposed Solution / What we do */}
+        <section></section>
+        {/* Our Team */}
+        <section></section>
+      </main>
+      {/* Contact us */}
+      <footer></footer>
+    </>
+  );
 };
 
 export default LandingPage;
