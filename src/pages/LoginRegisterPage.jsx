5f688332f4ebde602af61b4911a3044adb49aca3
Aman Pandagre <amanpandagre21@gmail.com>
2022-11-04 15:24:21 +0530

    AP - setup login and registration page

--- src/pages/LoginRegisterPage.jsx
+++ src/pages/LoginRegisterPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const LoginRegisterPage = () => {
-  return <div>LoginRegisterPage</div>;
+  return (
+    <section>
+      <div className="grid grid-cols-2 gap-4">
+        {/* input fields column */}
+        <div>
+          <h1>Login</h1>
+          <p>
+            Dont have any account ? <span>Register</span>
+          </p>
+          <div className="inputFields"></div>
+        </div>
+
+        {/* image column */}
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default LoginRegisterPage;
