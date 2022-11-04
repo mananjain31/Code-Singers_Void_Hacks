bc1bade6a19e53006bd1cc4788a89aad1e64b573
mananjain31 <mananjain31jan@gmail.com>
2022-11-04 13:41:51 +0530

    MJ - Api documentation

---
+++ api-documentation/register.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // all required
+  name: "Manan Jain",
+  contact: "9087656737", // 10 digit
+  email: "mananjain31sdasdasdasbvjh2@gmail.com", // email
+  address: {
+    pincode: "452010", // 6 digit
+    addressLine: "CM-123",
+    locality: "Sukhliya",
+    city: "Jaipur",
+    state: "Rajasthan",
+    country: "India",
+  },
+  password: "pw",
+};
+// login from mobile or email
