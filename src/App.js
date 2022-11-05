6f96d2e8cddb7a5a44e57a633eb3f45b4ff306eb
Aman Pandagre <amanpandagre21@gmail.com>
2022-11-05 10:31:33 +0530

    AP - fix error

--- src/App.js
+++ src/App.js
@@ -29,8 +29,6 @@ function App() {
           path="/dashboard"
           element={<RequireAuth>Protected</RequireAuth>}
         />
-        <Route path="/eWaste-timeline" element={<Timeline />} />
-        <Route path="/eWaste-upload" element={<WasteUpload />} />
       </Routes>
     </BrowserRouter>
   );
