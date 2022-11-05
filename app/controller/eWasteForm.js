9ae9704e5251037bd19cae5872d885fb2f18e0b3
aditya <adichoudhary2222@gmail.com>
2022-11-05 10:27:12 +0530

    added cloudinary

--- app/controller/eWasteForm.js
+++ app/controller/eWasteForm.js
@@ -1,13 +1,28 @@
 const EWaste = require("../models/eWaste");
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const eWasteInfo = async (req, res, next) => {
   try {
+    console.log(req.body);
     if (req.body.title) {
       const newPost = new EWaste();
       newPost.title = req.body.title;
       newPost.description = req.body.description;
+      const { image } = req.body.image;
+      const imageOnCloud = await cloudinary.v2.uploader.upload(image, {
+        folder: "/voidtrash",
+      });
       //console.log(req.file);
-      newPost.image = req.file.filename;
+      newPost.image = {
+        public_id: imageOnCloud.public_id,
+        url: imageOnCloud.secure_url,
+      };
       //console.log(req.userId);
       newPost.user = req.userId;
       await newPost.save();
--- app/models/eWaste.js
+++ app/models/eWaste.js
@@ -5,7 +5,10 @@ require("../models/user");
 const eWasteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
-  image: { type: String, required: true },
+  image: {
+    public_id: String,
+    url: String,
+  },
   user: { type: Schema.Types.ObjectId, ref: "users" },
 });
 
